43f0e1b506c6706cdc8bf4d3aee836e730594043
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-07 18:25:44 +0900

    chore: check hydration error

--- vue-server/src/demo/entry-browser.tsx
+++ vue-server/src/demo/entry-browser.tsx
@@ -11,7 +11,25 @@ function main() {
 	const app = createSSRApp(Root);
 	const el = document.getElementById("root");
 	tinyassert(el);
+
+	listenHydrationMismatch(() => {
+		document.title = "🚨 HYDRATE ERROR";
+	});
 	app.mount(el);
+}
+
+// patch console to notify hydration error
+function listenHydrationMismatch(f: () => void) {
+	const prev = console.error;
+	console.error = function (...args) {
+		if (
+			typeof args[0] === "string" &&
+			args[0].includes("Hydration completed but contains mismatches")
+		) {
+			f();
+		}
+		prev.apply(this, args);
+	};
 }
 
 main();
