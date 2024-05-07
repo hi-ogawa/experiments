43c3784c87387f08f1aa6196e98f2a8aa1bc31be
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-07 17:47:02 +0900

    chore: deploy cloudflare

---
+++ vue-server/misc/cloudflare-workers/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu -o pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# clean
+rm -rf dist
+mkdir -p dist/server dist/client
+
+# static
+cp -r ../../dist/client/. dist/client
+rm -rf dist/client/index.html
+
+# server (bundle by ourselve instead of relying on wrangler)
+npx esbuild ../../dist/server/index.js \
+  --outfile=dist/server/index.js \
+  --metafile=dist/esbuild-metafile.json \
+  --define:process.env.NODE_ENV='"production"' \
+  --define:__VUE_OPTIONS_API__='false' \
+  --define:__VUE_PROD_DEVTOOLS__='false' \
+  --define:__VUE_PROD_HYDRATION_MISMATCH_DETAILS__='false' \
+  --log-override:ignored-bare-import=silent \
+  --bundle \
+  --format=esm \
+  --platform=browser
