32976b97617094a126e39ac03caefbc4888ef82e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-09 17:24:38 +0900

    chore(deps): shiki v1.5.1

--- vue-server/misc/cloudflare-workers/build.sh
+++ vue-server/misc/cloudflare-workers/build.sh
@@ -15,8 +15,8 @@ rm -rf dist/client/index.html
 npx esbuild ../../dist/server/index.js \
   --outfile=dist/server/index.js \
   --metafile=dist/esbuild-metafile.json \
-  --alias:shiki/wasm=./alias-shiki-wasm.mjs \
-  --external:./shiki.wasm \
+  --alias:shiki/wasm=../../../../node_modules/shiki/dist/onig.wasm \
+  --external:../../../../node_modules/shiki/dist/onig.wasm \
   --define:process.env.NODE_ENV='"production"' \
   --define:__VUE_OPTIONS_API__='false' \
   --define:__VUE_PROD_DEVTOOLS__='false' \
@@ -25,6 +25,3 @@ npx esbuild ../../dist/server/index.js \
   --bundle \
   --format=esm \
   --platform=browser
-
-# copy shiki wasm
-cp ../../node_modules/shiki/dist/onig.wasm dist/server/shiki.wasm
