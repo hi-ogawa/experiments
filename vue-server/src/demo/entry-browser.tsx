9e60d44a651a6165318be007d6ada7ae48c9e596
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-20 09:27:06 +0900

    refactor: minor

--- vue-server/src/demo/entry-browser.tsx
+++ vue-server/src/demo/entry-browser.tsx
@@ -1,6 +1,12 @@
 import "./style.css";
 import { tinyassert } from "@hiogawa/utils";
-import { createSSRApp, defineComponent, provide, readonly, ref } from "vue";
+import {
+	createSSRApp,
+	defineComponent,
+	provide,
+	readonly,
+	shallowRef,
+} from "vue";
 import { type SerializeResult, deserialize } from "../serialize";
 import { createReferenceMap } from "./integrations/client-reference/runtime";
 import { listenBrowserHistory } from "./integrations/router/browser";
@@ -11,36 +17,35 @@ async function main() {
 	}
 
 	const initResult: SerializeResult = (globalThis as any).__serialized;
-	const referenceMap = await createReferenceMap(initResult.referenceIds);
-
-	async function fetchSerialize() {
-		const url = new URL(window.location.href);
-		url.searchParams.set("__serialize", "");
-		const res = await fetch(url);
-		tinyassert(res.ok);
-		const result: SerializeResult = await res.json();
-		Object.assign(referenceMap, await createReferenceMap(result.referenceIds));
-		return result;
-	}
+	const initReferenceMap = await createReferenceMap(initResult.referenceIds);
+	const initRender = () => deserialize(initResult.data, initReferenceMap);
 
 	const Root = defineComponent(() => {
-		const serialized = ref(initResult);
-		const isLoading = ref(false);
+		const render = shallowRef(initRender);
+		const isLoading = shallowRef(false);
 		provide("isLoading", readonly(isLoading));
 
-		const navManager = new AsyncTaskManager<SerializeResult>({
+		const navManager = new AsyncTaskManager<() => void>({
 			onSucess: (result) => {
-				serialized.value = result;
+				render.value = result;
 				isLoading.value = false;
 			},
 		});
 
 		listenBrowserHistory(() => {
 			isLoading.value = true;
-			navManager.push(fetchSerialize);
+			navManager.push(async () => {
+				const url = new URL(window.location.href);
+				url.searchParams.set("__serialize", "");
+				const res = await fetch(url);
+				tinyassert(res.ok);
+				const result: SerializeResult = await res.json();
+				const referenceMap = await createReferenceMap(result.referenceIds);
+				return () => deserialize(result.data, referenceMap);
+			});
 		});
 
-		return () => deserialize(serialized.value.data, referenceMap) as any;
+		return () => render.value() as any;
 	});
 
 	const app = createSSRApp(Root);
