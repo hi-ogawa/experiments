402bc207a8e78c2a1a7f0f90d03bb971c1defe65
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-21 11:44:20 +0900

    wip: cleanup

--- tsr-rsc/src/routes/index.tsx
+++ tsr-rsc/src/routes/index.tsx
@@ -5,13 +5,18 @@ import { reviveFlightClient } from "../integrations/server-component/client";
 
 // TODO: whole loader should be extracted and executed on react server environment.
 // TODO: also auto proxy on browser
+const wip = {
+	loader: async () => {
+		"use server";
+		await sleep(300);
+		return <div>server random: {Math.random().toString(36).slice(2)}</div>;
+	},
+};
+
 export async function flightLoader() {
 	if (import.meta.env.SSR) {
 		const { flightSsr } = await import("../integrations/server-component/ssr");
-		const data = await flightSsr(
-			<div>server random: {Math.random().toString(36).slice(2)}</div>,
-		);
-		return data;
+		return flightSsr(wip.loader());
 	} else {
 		const res = await fetch("/__flight");
 		tinyassert(res.ok);
@@ -24,7 +29,6 @@ export const Route = createFileRoute("/")({
 		// TODO: helper
 		const data = await flightLoader();
 		const revived = (await reviveFlightClient(data)) as React.ReactNode;
-		await sleep(300);
 		return { ...data, revived };
 	},
 	component: IndexComponent,
