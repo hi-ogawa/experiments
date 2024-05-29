e5dc4afd5b32cd9d22b38d9cd1240ead40e8aa25
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-29 09:17:07 +0900

    chore(deps): use transforms package (#17)

--- vue-server/src/demo/integrations/client-reference/plugin-utils.ts
+++ vue-server/src/demo/integrations/client-reference/plugin-utils.ts
@@ -1,138 +1,31 @@
-import { tinyassert } from "@hiogawa/utils";
+import { getExportNames, transformWrapExport } from "@hiogawa/transforms";
 import { type Plugin, parseAstAsync } from "vite";
-import { MagicString } from "vue/compiler-sfc";
 
 export async function transformClientReference(input: string, id: string) {
-	const { output } = await transformWrapExports(
-		input,
-		(expr, name) => `$$wrap((${expr}), ${JSON.stringify(id + "#" + name)})`,
-	);
-	output.prepend(
-		`import { registerClientReference as $$wrap } from "/src/serialize";\n`,
-	);
+	const ast = await parseAstAsync(input);
+	const { output } = await transformWrapExport(input, ast, {
+		id,
+		runtime: "$$wrap",
+		ignoreExportAllDeclaration: true,
+	});
+	output.prepend(`\
+import { registerClientReference as $$wrap1 } from "/src/serialize";
+const $$wrap = (v, id, name) => $$wrap1(v, id + "#" + name);
+`);
 	return output;
 }
 
 export async function transformEmptyExports(input: string) {
-	const { exportNames } = await transformWrapExports(input, () => "");
+	const ast = await parseAstAsync(input);
+	const { exportNames } = getExportNames(ast, {
+		ignoreExportAllDeclaration: true,
+	});
 	const stmts = exportNames.map((name) =>
 		name === "default"
 			? "export default undefined"
 			: `export const ${name} = undefined`,
 	);
 	return [...stmts, ""].join(";\n");
-}
-
-// extend types for rollup ast with node position
-declare module "estree" {
-	interface BaseNode {
-		start: number;
-		end: number;
-	}
-}
-
-async function transformWrapExports(
-	input: string,
-	wrap: (expr: string, name: string) => string,
-) {
-	const ast = await parseAstAsync(input);
-	const output = new MagicString(input);
-	const exportNames: string[] = [];
-	const toBeAppended: string[] = [];
-
-	function wrapExport(name: string, exportName = name) {
-		exportNames.push(exportName);
-		toBeAppended.push(
-			`const $$tmp_${name} = ${wrap(name, exportName)}`,
-			`export { $$tmp_${name} as ${exportName} }`,
-		);
-	}
-
-	for (const node of ast.body) {
-		// named exports
-		if (node.type === "ExportNamedDeclaration") {
-			if (node.declaration) {
-				if (
-					node.declaration.type === "FunctionDeclaration" ||
-					node.declaration.type === "ClassDeclaration"
-				) {
-					/**
-					 * export function foo() {}
-					 */
-					output.remove(node.start, node.start + 6);
-					wrapExport(node.declaration.id.name);
-				} else if (node.declaration.type === "VariableDeclaration") {
-					/**
-					 * export const foo = 1, bar = 2
-					 */
-					output.remove(node.start, node.start + 6);
-					for (const decl of node.declaration.declarations) {
-						tinyassert(decl.id.type === "Identifier");
-						wrapExport(decl.id.name);
-					}
-				} else {
-					node.declaration satisfies never;
-				}
-			} else {
-				if (node.source) {
-					/**
-					 * export { foo, bar as car } from './foo'
-					 */
-					output.remove(node.start, node.end);
-					for (const spec of node.specifiers) {
-						const name = spec.local.name;
-						toBeAppended.push(
-							`import { ${name} as $$import_${name} } from ${node.source.raw}`,
-						);
-						wrapExport(`$$import_${name}`, spec.exported.name);
-					}
-				} else {
-					/**
-					 * export { foo, bar as car }
-					 */
-					output.remove(node.start, node.end);
-					for (const spec of node.specifiers) {
-						wrapExport(spec.local.name, spec.exported.name);
-					}
-				}
-			}
-		}
-
-		/**
-		 * export * from './foo'
-		 */
-		if (node.type === "ExportAllDeclaration") {
-			// vue sfc uses this to re-export setup script. for now we can ignore.
-			// https://github.com/vitejs/vite-plugin-vue/blob/30a97c1ddbdfb0e23b7dc14a1d2fb609668b9987/packages/plugin-vue/src/main.ts#L372
-		}
-
-		/**
-		 * export default function foo() {}
-		 * export default class Foo {}
-		 * export default () => {}
-		 */
-		if (node.type === "ExportDefaultDeclaration") {
-			let localName: string;
-			if (
-				(node.declaration.type === "FunctionDeclaration" ||
-					node.declaration.type === "ClassDeclaration") &&
-				node.declaration.id
-			) {
-				// preserve name scope for `function foo() {}` and `class Foo {}`
-				localName = node.declaration.id.name;
-				output.remove(node.start, node.declaration.start);
-			} else {
-				// otherwise introduce new variable
-				localName = "$$default";
-				output.update(node.start, node.declaration.start, "const $$default = ");
-			}
-			wrapExport(localName, "default");
-		}
-	}
-
-	output.append(["", ...toBeAppended, ""].join(";\n"));
-
-	return { exportNames, output };
 }
 
 export function createVirtualPlugin(name: string, load: Plugin["load"]) {
