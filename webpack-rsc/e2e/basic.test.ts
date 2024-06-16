f2c8c200f2c2d3acac25085a68aa2a0dd0295216
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-16 18:26:26 +0900

    test: e2e client components

--- webpack-rsc/e2e/basic.test.ts
+++ webpack-rsc/e2e/basic.test.ts
@@ -1,8 +1,9 @@
 import { test } from "@playwright/test";
-import { testNoJs } from "./helper";
+import { testNoJs, waitForHydration } from "./helper";
 
 test("basic @js", async ({ page }) => {
 	await page.goto("/");
+	await waitForHydration(page);
 	await page.getByRole("heading", { name: "Webpack RSC" }).click();
 });
 
@@ -16,4 +17,11 @@ test("stream", async ({ page }) => {
 	await page.getByText("Outer").click();
 	await page.getByText("Sleeping 1 sec").click();
 	await page.getByText("Inner").click();
+});
+
+test("client", async ({ page }) => {
+	await page.goto("/");
+	await waitForHydration(page);
+	await page.getByRole("button", { name: "count is 0" }).click();
+	await page.getByRole("button", { name: "count is 1" }).click();
 });
