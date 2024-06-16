b98f5b760d44b86dee2b040434e9e33eea0aca6d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-16 14:02:37 +0900

    test: e2e stream

--- webpack-rsc/e2e/basic.test.ts
+++ webpack-rsc/e2e/basic.test.ts
@@ -10,3 +10,10 @@ testNoJs("basic @nojs", async ({ page }) => {
 	await page.goto("/");
 	await page.getByRole("heading", { name: "Webpack RSC" }).click();
 });
+
+test("stream", async ({ page }) => {
+	await page.goto("/stream", { waitUntil: "commit" });
+	await page.getByText("Outer").click();
+	await page.getByText("Sleeping 1 sec").click();
+	await page.getByText("Inner").click();
+});
