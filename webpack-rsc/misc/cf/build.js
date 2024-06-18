30ce34340993b59e24406730a4a1e028c4ae7c05
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-18 11:11:25 +0900

    feat: cf deploy

---
+++ webpack-rsc/misc/cf/build.js
@@ -0,0 +1,70 @@
+import { cp, mkdir, rm, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import * as esbuild from "esbuild";
+
+const buildDir = join(import.meta.dirname, "../../dist");
+const outDir = join(import.meta.dirname, "dist");
+
+async function main() {
+	// clean
+	await rm(outDir, { recursive: true, force: true });
+	await mkdir(outDir, { recursive: true });
+
+	// assets
+	await cp(join(buildDir, "browser"), outDir, {
+		recursive: true,
+	});
+
+	// worker routes
+	// https://developers.cloudflare.com/pages/functions/routing/#create-a-_routesjson-file
+	await writeFile(
+		join(outDir, "_routes.json"),
+		JSON.stringify(
+			{
+				version: 1,
+				include: ["/*"],
+				exclude: ["/favicon.ico", "/assets/*"],
+			},
+			null,
+			2,
+		),
+	);
+
+	// headers
+	// https://developers.cloudflare.com/pages/configuration/headers/
+	await writeFile(
+		join(outDir, "_headers"),
+		`\
+/favicon.ico
+  Cache-Control: public, max-age=3600, s-maxage=3600
+/assets/*
+  Cache-Control: public, max-age=31536000, immutable
+`,
+	);
+
+	// worker
+	// https://developers.cloudflare.com/pages/functions/advanced-mode/
+	await writeFile(
+		join(buildDir, "server/__cf.js"),
+		`\
+import server from "./index.cjs";
+export default { fetch: server.handler };
+`,
+	);
+	await esbuild.build({
+		entryPoints: [join(buildDir, "server/__cf.js")],
+		outfile: join(outDir, "_worker.js"),
+		bundle: true,
+		minify: true,
+		format: "esm",
+		platform: "browser",
+		define: {
+			"process.env.NODE_ENV": `"production"`,
+		},
+		logOverride: {
+			"ignored-bare-import": "silent",
+		},
+	});
+}
+
+main();
