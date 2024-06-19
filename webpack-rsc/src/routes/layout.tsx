a6392286280b416d26cde1ac4b090063d493a830
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-19 10:19:09 +0900

    fix: add meta viewport

--- webpack-rsc/src/routes/layout.tsx
+++ webpack-rsc/src/routes/layout.tsx
@@ -8,6 +8,7 @@ export default function Layout(props: React.PropsWithChildren) {
 				<meta charSet="utf-8" />
 				<title>Webpack RSC</title>
 				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<style>{css}</style>
 			</head>
 			<body>
