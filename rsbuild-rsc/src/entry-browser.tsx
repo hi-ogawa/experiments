003f881daf65c64b20df986f2341d9a0144a34d0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-27 11:36:23 +0900

    feat: basic build

--- rsbuild-rsc/src/entry-browser.tsx
+++ rsbuild-rsc/src/entry-browser.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { App } from "./app";
 import ReactDOMClient from "react-dom/client";
 
 function main() {
 	React.startTransition(() => {
 		ReactDOMClient.hydrateRoot(
 			document.body,
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>,
+			<React.StrictMode></React.StrictMode>,
 		);
 	});
 }
--- rsbuild-rsc/src/entry-ssr.tsx
+++ rsbuild-rsc/src/entry-ssr.tsx
@@ -1,47 +1,23 @@
-import type { ServerAPIs, Rspack } from "@rsbuild/core";
-import { App } from "./app";
+import type { ServerAPIs } from "@rsbuild/core";
 import ReactDOMServer from "react-dom/server.edge";
 import ReactClient from "react-server-dom-webpack/client.edge";
-import { tinyassert } from "@hiogawa/utils";
 import type { FlightData } from "./entry-server";
 
 declare let __rsbuild_server__: ServerAPIs;
 
 export default async function handler(request: Request): Promise<Response> {
-	if (1) {
-		const reactServer = await importReactServer();
-		const { flightStream } = await reactServer.handler(request);
+	const reactServer = await importReactServer();
+	const { flightStream } = await reactServer.handler(request);
 
-		const flightData = await ReactClient.createFromReadableStream<FlightData>(
-			flightStream,
-			{ ssrManifest: { moduleMap: {} } },
-		);
+	const flightData = await ReactClient.createFromReadableStream<FlightData>(
+		flightStream,
+		{ ssrManifest: { moduleMap: {} } },
+	);
 
-		const ssrRoot = <>{flightData.node}</>;
-		const htmlStream = await ReactDOMServer.renderToReadableStream(ssrRoot, {});
-		return new Response(htmlStream, {
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-	}
-
-	const statsJson = await getStatsJson();
-	let scripts: string[] = [];
-	let styles: string[] = [];
-	tinyassert(statsJson.assets);
-	for (const { name } of statsJson.assets) {
-		if (name.endsWith(".js") && !name.includes(".hot-update.js")) {
-			scripts.push(`/${name}`);
-		}
-		if (name.endsWith(".css")) {
-			styles.push(`/${name}`);
-		}
-	}
-
-	const root = <Root styles={styles} />;
-	const htmlStream = await ReactDOMServer.renderToReadableStream(root, {
-		bootstrapScripts: scripts,
+	const ssrRoot = <>{flightData.node}</>;
+	const htmlStream = await ReactDOMServer.renderToReadableStream(ssrRoot, {
+		// TODO
+		bootstrapModules: [],
 	});
 	return new Response(htmlStream, {
 		headers: {
@@ -54,36 +30,9 @@ async function importReactServer(): Promise<typeof import("./entry-server")> {
 	if (import.meta.env.DEV) {
 		return __rsbuild_server__.environments.server.loadBundle("index");
 	} else {
-		throw "todo";
-	}
-}
-
-async function getStatsJson(): Promise<Rspack.StatsCompilation> {
-	if (import.meta.env.DEV) {
-		const stats = await __rsbuild_server__.environments.web.getStats();
-		return stats.toJson();
-	} else {
-		const { default: statsJson } = await import(
-			/* webpackIgnore: true */ "../client/stats.json" as string,
-			{ with: { type: "json" } }
+		const mod = await import(
+			/* webpackIgnore: true */ "../server/index.cjs" as string
 		);
-		return statsJson;
+		return mod.default;
 	}
-}
-
-function Root(props: { styles: string[] }) {
-	return (
-		<html>
-			<head>
-				<meta charSet="utf-8" />
-				<title>Rsbuild React SSR</title>
-				{props.styles.map((href) => (
-					<link key={href} rel="stylesheet" href={href} />
-				))}
-			</head>
-			<body>
-				<App />
-			</body>
-		</html>
-	);
 }
