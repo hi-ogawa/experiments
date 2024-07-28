484d0fec1ad727b4101a972d65cb316fbad53061
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-28 13:14:30 +0900

    chore: fix preview

--- rsbuild-rsc/vite.config.ts
+++ rsbuild-rsc/vite.config.ts
@@ -5,10 +5,17 @@ import { defineConfig } from "vite";
 // use vite preview server for local build
 
 export default defineConfig({
+	appType: "custom",
 	plugins: [
 		{
 			name: "preview-middleware",
 			async configurePreviewServer(server) {
+				server.middlewares.use((req, _res, next) => {
+					// disable compression entirely since it breaks streaming
+					delete req.headers["accept-encoding"];
+					next();
+				});
+
 				const mod = await import(path.resolve("./dist/ssr/index.cjs"));
 				return () => {
 					server.middlewares.use(webToNodeHandler(mod.default.default));
