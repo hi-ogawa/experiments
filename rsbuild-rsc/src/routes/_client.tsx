d83f404de257a700802254278fbace7c937b142c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-28 12:22:15 +0900

    chore: tweak demo

--- rsbuild-rsc/src/routes/_client.tsx
+++ rsbuild-rsc/src/routes/_client.tsx
@@ -8,3 +8,15 @@ export function Counter() {
 		<button onClick={() => setCount((v) => v + 1)}>Count is {count}</button>
 	);
 }
+
+export function Hydrated() {
+	return <pre>[hydrated: {Number(useHydrated())}]</pre>;
+}
+
+function useHydrated() {
+	return React.useSyncExternalStore(
+		React.useCallback(() => () => {}, []),
+		() => true,
+		() => false,
+	);
+}
--- rsbuild-rsc/src/routes/page.tsx
+++ rsbuild-rsc/src/routes/page.tsx
@@ -7,12 +7,24 @@ const Counter = ReactServer.registerClientReference(
 	"Counter",
 ) as React.ComponentType;
 
+const Hydrated = ReactServer.registerClientReference(
+	{},
+	"/home/hiroshi/code/personal/experiments/rsbuild-rsc/src/routes/_client.tsx",
+	"Hydrated",
+) as React.ComponentType;
+
 export default function Page() {
 	return (
 		<div id="root">
 			<h1>Rsbuild RSC</h1>
 			<pre>{React.version}</pre>
+			<Hydrated />
 			<Counter />
+			<p className="read-the-docs">
+				<a href="https://github.com/hi-ogawa/experiments/tree/main/rsbuild-rsc">
+					View code on GitHub
+				</a>
+			</p>
 		</div>
 	);
 }
