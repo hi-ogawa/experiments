885a79b4eb62da3fdcef1c0090c4e53de4f0e60d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-17 17:45:58 +0900

    chore: tweak

--- oxc-nft/src/lib.rs
+++ oxc-nft/src/lib.rs
@@ -1,13 +1,3 @@
 pub fn add(x: isize, y: isize) -> isize {
     x + y
 }
-
-#[cfg(test)]
-mod tests {
-    use super::add;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-}
---
+++ oxc-nft/tests/basic.rs
@@ -0,0 +1,4 @@
+#[test]
+fn test_add() {
+    assert_eq!(oxc_nft::add(1, 2), 3);
+}
