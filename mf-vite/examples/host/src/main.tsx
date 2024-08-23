818e737a4e685de3306664c953eb5ac51e2d1b6e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-23 15:58:07 +0900

    wip: simple

--- mf-vite/examples/host/src/main.tsx
+++ mf-vite/examples/host/src/main.tsx
@@ -1,9 +1,27 @@
+import { init, loadRemote } from "@module-federation/runtime";
 import ReactDOMClient from "react-dom/client";
 import { App } from "./app";
 
-function main() {
+async function main() {
+	init({
+		name: "host",
+		remotes: [
+			{
+				name: "remote-simple",
+				entry: "http://localhost:5000/simple.js",
+			},
+		],
+		shared: {},
+	});
+	await loadRemote("remote-simple");
+
 	const el = document.getElementById("app")!;
 	ReactDOMClient.createRoot(el).render(<App />);
 }
 
-main();
+main().catch((e) => {
+	const el = document.createElement("pre");
+	el.style.color = "red";
+	el.textContent = e.stack;
+	document.body.appendChild(el);
+});
