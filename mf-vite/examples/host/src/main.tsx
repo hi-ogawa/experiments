c1fc409cd03d4121f1a48f8b4f963e806e3d1172
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-23 16:58:27 +0900

    wip: manifest

--- mf-vite/examples/host/src/main.tsx
+++ mf-vite/examples/host/src/main.tsx
@@ -8,21 +8,28 @@ async function main() {
 		remotes: [
 			{
 				name: "simple-esm",
-				entry: "http://localhost:5000/simple/esm.js",
+				entry: "http://localhost:5000/simple-esm/entry.js",
 				// RemoteEntryType
 				// https://github.com/module-federation/core/blob/b90fa7ded8786022d900081dd7c871f317c5e4b9/packages/runtime/src/type/config.ts#L17
 				type: "module",
+			},
+			{
+				name: "simple-manifest",
+				entry: "http://localhost:5000/simple-manifest/mf-manifest.json",
 			},
 		],
 		shared: {},
 	});
 	const remoteSimpleEsm = await loadRemote<any>("simple-esm");
+	// const remoteSimpleManifest = await loadRemote<any>("simple-manifest");
+	// console.log(remoteSimpleManifest);
 
 	const el = document.getElementById("app")!;
 	ReactDOMClient.createRoot(el).render(
 		<>
 			<App />
 			<pre>remoteSimpleEsm.add(1, 2) = {remoteSimpleEsm.add(1, 2)}</pre>
+			{/* <pre>remoteSimpleManifest.add(1, 2) = {remoteSimpleManifest.add(1, 2)}</pre> */}
 		</>,
 	);
 }
