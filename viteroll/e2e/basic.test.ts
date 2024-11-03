f9888127b0b5f5b451eebb5cc430c481e4a4d225
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-03 18:59:46 +0900

    test: add e2e

---
+++ viteroll/e2e/basic.test.ts
@@ -0,0 +1,43 @@
+import { test } from "@playwright/test";
+import { createEditor } from "./helper";
+
+test("basic", async ({ page }) => {
+	await page.goto("/");
+	await page.getByRole("heading", { name: "Test" }).click();
+	await page.getByText("[define] ok").click();
+	await page.getByText("[virtual:test] ok").click();
+
+	await page.getByRole("button", { name: "Count: 0" }).click();
+	await page.getByRole("button", { name: "Count: 1" }).click();
+});
+
+test("reload", async ({ page }) => {
+	await page.goto("/");
+
+	await page.getByRole("heading", { name: "Test" }).click();
+	await page.getByRole("button", { name: "Count: 0" }).click();
+	await page.getByRole("button", { name: "Count: 1" }).click();
+
+	using file = createEditor("./src/index.ts");
+	file.edit((s) => s.replace('h("h3", {}, "Test")', 'h("h3", {}, "Teditst")'));
+
+	await page.getByRole("heading", { name: "Teditst" }).click();
+	await page.getByRole("button", { name: "Count: 0" }).click();
+});
+
+test("hmr", async ({ page }) => {
+	await page.goto("/");
+
+	await page.getByText("[dep-hmr.ts] Edit me!").click();
+	await page.getByRole("button", { name: "Count: 0" }).click();
+
+	using file = createEditor("./src/dep-hmr.ts");
+	file.edit((s) => s.replace("Edit me!", "Edit foo!"));
+
+	await page.getByText("[dep-hmr.ts] Edit foo!").click();
+	await page.getByRole("button", { name: "Count: 1" }).click();
+
+	file.edit((s) => s.replace("Edit foo!", "Edit bar!"));
+	await page.getByText("[dep-hmr.ts] Edit bar!").click();
+	await page.getByRole("button", { name: "Count: 2" }).click();
+});
---
+++ viteroll/playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const port = Number(process.env.E2E_PORT || 6174);
+const command = `pnpm dev --port ${port} --strict-port`;
+
+export default defineConfig({
+	testDir: "e2e",
+	use: {
+		trace: "on-first-retry",
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: {
+				...devices["Desktop Chrome"],
+				viewport: null,
+				deviceScaleFactor: undefined,
+			},
+		},
+	],
+	webServer: {
+		command,
+		port,
+	},
+	forbidOnly: !!process.env["CI"],
+	retries: process.env["CI"] ? 2 : 0,
+	reporter: "list",
+});
