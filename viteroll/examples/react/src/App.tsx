ec0a2df3dcf2d4f6ea40b4d4e918dbf668f163cd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-03 19:13:14 +0900

    test: more e2e

--- viteroll/examples/react/src/App.tsx
+++ viteroll/examples/react/src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
+					Count: {count}
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
