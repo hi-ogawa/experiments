ef11da99060702304de005352082b0ec49566af9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-06 19:11:30 +0900

    chore: hmr demo

--- viteroll/examples/ssr/src/entry-server.ts
+++ viteroll/examples/ssr/src/entry-server.ts
@@ -7,17 +7,24 @@ const handler: Connect.SimpleHandleFunction = (req, res) => {
 	// TODO: transformIndexHtml?
 	res.end(`\
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>viteroll ssr</title>
-  </head>
-  <body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<script type="module">
+			import { createHotContext } from "/@vite/client";
+			const hot = createHotContext("/__rolldown");
+			hot.on("rolldown:hmr", (data) => {
+				(0, eval)(data[1]);
+			});
+			window.__rolldown_hot = hot;
+		</script>
+	</head>
+	<body>
 		<h1>Rolldown SSR</h1>
 		<div>pathname: ${url.pathname}</div>
 		<div id="root"></div>
-    <script src="/entry-client.js"></script>
-  </body>
+		<script src="/entry-client.js"></script>
+	</body>
 </html>
 `);
 };
