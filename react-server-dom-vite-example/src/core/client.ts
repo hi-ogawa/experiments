031868f9a9ad534fddd83447f598566481fd07af
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-02 13:21:00 +0900

    chore: comment

--- react-server-dom-vite-example/src/core/client.ts
+++ react-server-dom-vite-example/src/core/client.ts
@@ -8,6 +8,10 @@ export const clientReferenceManifest: ClientReferenceManifest = {
 			async preload() {
 				let mod: Record<string, unknown>;
 				if (import.meta.env.DEV) {
+					// TODO: this one can still break module identity due to `?import`
+					// TODO: also in practice, this `id` needs same normalization as vite's import analaysis.
+					// TODO: after doing all that, there are still issues related to external dependency and optimize deps.
+					// should such "feature" land on Vite level or worked around as Core plugin?
 					mod = await import(/* @vite-ignore */ id);
 				} else {
 					// @ts-ignore
