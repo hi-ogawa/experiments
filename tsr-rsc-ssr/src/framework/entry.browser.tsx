a46e4d8f697f9ea61ea386cbc9c96fd999089eba
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-02 18:22:54 +0900

    fix: fix hydration (use RouterClient)

--- tsr-rsc-ssr/src/framework/entry.browser.tsx
+++ tsr-rsc-ssr/src/framework/entry.browser.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
+import { RouterClient } from "@tanstack/react-router/ssr/client";
 import { createRouter } from "../router";
 
 function main() {
   const router = createRouter();
-  ReactDOM.hydrateRoot(document, <RouterProvider router={router} />);
+  ReactDOM.hydrateRoot(document, <RouterClient router={router} />);
 }
 
 main();
