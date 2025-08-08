901ad18e78dd5d74c94fd7c3fdf5002e68428a86
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-08 15:26:53 +0900

    chore: remove old tanstack example

--- tsr-rsc/e2e/helper.ts
+++
@@ -1,9 +0,0 @@
-import test, { type Page, expect } from "@playwright/test";
-
-export const testNoJs = test.extend({
-	javaScriptEnabled: ({}, use) => use(false),
-});
-
-export async function waitForHydration(page: Page) {
-	await expect(page.getByText("[hydrated: 1]")).toBeVisible();
-}
--- tsr-rsc/src/helper.tsx
+++
@@ -1,5 +0,0 @@
-import { useLoaderData } from "@tanstack/react-router";
-
-export function LoaderDataComponent() {
-	return useLoaderData({ strict: false });
-}
